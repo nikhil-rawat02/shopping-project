4c928d63c412c7d19c2eb16c3628403b38cf6958
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-06-02 16:26:01 +0530

    razorpay createorder testing

--- razorpay/razorpay.js
+++ razorpay/razorpay.js
@@ -7,28 +7,8 @@ const userMobile = user.userMobile;
 function getOrderId() {
     
 }
-var options = {
-    "key": "rzp_test_Ye8PvD7HIK3Mcb", // Enter the Key ID generated from the Dashboard
-    "amount": ammout, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
-    "currency": "INR",
-    "name": "MeShop.", //your business name
-    "description": "Test Transaction",
-    "image": "../images/home.png",
-    "order_id": order_id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-    "callback_url": "https://eneqd3r9zrjok.x.pipedream.net/",
-    "prefill": { //We recommend using the prefill parameter to auto-fill customer's contact information especially their phone number
-        "name": userName, //your customer's name
-        "email": userEmail,
-        "contact": userMobile //Provide the customer's phone number for better conversion rates 
-    },
-    "notes": {
-        "address": "Razorpay Corporate Office"
-    },
-    "theme": {
-        "color": "#3399cc"
-    }
-};
-var payment = new Razorpay(options);
+
+
 const btn = document.getElementById("checkout_button_container");
 
 btn.addEventListener("click", async (e) => {
@@ -39,23 +19,48 @@ btn.addEventListener("click", async (e) => {
     localStorage.setItem("curretUserCart", JSON.stringify(alluserCart));
     loadCartUI();
     e.preventDefault();
-    if(ammout == 0){
-        alert("add item in cart for payment");
-        return;
-    }
-    var instance = new Razorpay({ key_id: 'rzp_test_Ye8PvD7HIK3Mcb', key_secret: 'qo5sM7ASW1dhkffwf5M33Bl3' })
-    console.log("getting orderID from razor pay");
-    const response = await instance.orders.create({
-        amount: ammout,
-        currency: "INR",
-        receipt: "receipt#1",
-        notes: {
-            key1: "value3",
-            key2: "value2"
-        }
-    })
-    console.log(response);
-    console.log("api hitting");
+    // if(ammout == 0){
+    //     alert("add item in cart for payment");
+    //     return;
+    // }
+
+    // create order
+    
+    fetch('https://api.razorpay.com/v1/orders', {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+    'Authorization': 'Bearer XQKB3kvNAt9bhg9paBc6aqNh'
+  },
+  body: JSON.stringify({
+    amount: 1000,  // Order amount in paise (e.g., Rs. 10.00 = 1000)
+    currency: 'INR',  // Currency code
+    receipt: 'order_receipt'  // Receipt identifier for your reference
+  })
+})
+.then(response => response.json())
+.then(data => {
+  const orderId = data.id;
+  console.log('Order ID:', orderId);
+  // Use the order ID for further processing
+})
+.catch(error => {
+  console.error('Error creating order:', error);
+});
+
+    // var instance = new Razorpay({ key_id: 'rzp_test_Ye8PvD7HIK3Mcb', key_secret: 'qo5sM7ASW1dhkffwf5M33Bl3' })
+    // console.log("getting orderID from razor pay");
+    //  instance.orders.create({
+    //     amount: 5000,
+    //     currency: "INR",
+    //     receipt: "receipt#1",
+    //     notes: {
+    //         key1: "value3",
+    //         key2: "value2"
+    //     }
+    // })
+    // console.log(response);
+    // console.log("api hitting");
     // get orderid
     
     // do payment
