25dbe45cde56d05c4ece02f26e71692aa9a9bf87
nikhil-rawat02 <117645790+nikhil-rawat02@users.noreply.github.com>
2023-06-02 18:01:33 +0530

    check

--- razorpay/razorpay.js
+++ razorpay/razorpay.js
@@ -17,51 +17,53 @@ btn.addEventListener("click", async (e) => {
         alluserCart[currentUserEmail] = [];
         localStorage.setItem("curretUserCart", JSON.stringify(alluserCart));
         loadCartUI();
+        loadSummary();
         document.getElementById("total_ammount").innerText = 0;
+    }
  // create razorpay order
         
-    fetch('https://api.razorpay.com/v1/orders', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': 'Bearer XQKB3kvNAt9bhg9paBc6aqNh'
-        },
-        body: JSON.stringify({
-          amount: amount,  // Order amount in paise (e.g., Rs. 10.00 = 1000)
-          currency: 'INR',  // Currency code
-          receipt: 'order_receipt'  // Receipt identifier for your reference
-        })
-      })
-      .then(response => response.json())
-      .then(data => {
-        var options = {
-            "key": "XQKB3kvNAt9bhg9paBc6aqNh", // Enter the Key ID generated from the Dashboard
-            "amount": ammount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
-            "currency": "INR",
-            "name": "meShop", //your business name
-            "description": "Test Transaction",
-            "image": "../images/home.png",
-            "order_id": data.id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
-            "callback_url": "https://eneqd3r9zrjok.x.pipedream.net/",
-            "prefill": { //We recommend using the prefill parameter to auto-fill customer's contact information especially their phone number
-                "name": userName, //your customer's name
-                "email": userEmail,
-                "contact": userMobile //Provide the customer's phone number for better conversion rates 
-            },
-            "notes": {
-                "address": "Razorpay Corporate Office"
-            },
-            "theme": {
-                "color": "#3399cc"
-            }
-        };
-      })
-      .catch(error => {
-        console.error('Error creating order:', error);
+    // fetch('https://api.razorpay.com/v1/orders', {
+    //     method: 'POST',
+    //     headers: {
+    //       'Content-Type': 'application/json',
+    //       'Authorization': 'Bearer XQKB3kvNAt9bhg9paBc6aqNh'
+    //     },
+    //     body: JSON.stringify({
+    //       amount: amount,  // Order amount in paise (e.g., Rs. 10.00 = 1000)
+    //       currency: 'INR',  // Currency code
+    //       receipt: 'order_receipt'  // Receipt identifier for your reference
+    //     })
+    //   })
+    //   .then(response => response.json())
+    //   .then(data => {
+    //     var options = {
+    //         "key": "XQKB3kvNAt9bhg9paBc6aqNh", // Enter the Key ID generated from the Dashboard
+    //         "amount": ammount, // Amount is in currency subunits. Default currency is INR. Hence, 50000 refers to 50000 paise
+    //         "currency": "INR",
+    //         "name": "meShop", //your business name
+    //         "description": "Test Transaction",
+    //         "image": "../images/home.png",
+    //         "order_id": data.id, //This is a sample Order ID. Pass the `id` obtained in the response of Step 1
+    //         "callback_url": "https://eneqd3r9zrjok.x.pipedream.net/",
+    //         "prefill": { //We recommend using the prefill parameter to auto-fill customer's contact information especially their phone number
+    //             "name": userName, //your customer's name
+    //             "email": userEmail,
+    //             "contact": userMobile //Provide the customer's phone number for better conversion rates 
+    //         },
+    //         "notes": {
+    //             "address": "Razorpay Corporate Office"
+    //         },
+    //         "theme": {
+    //             "color": "#3399cc"
+    //         }
+    //     };
+    //   })
+    //   .catch(error => {
+    //     console.error('Error creating order:', error);
         
-        window.location.href("https://nikhil-rawat02.github.io/shopping-project/cart/cart.html")
-      });
-    }
+    //     window.location.href("https://nikhil-rawat02.github.io/shopping-project/cart/cart.html")
+    //   });
+    // }
 
    
     
